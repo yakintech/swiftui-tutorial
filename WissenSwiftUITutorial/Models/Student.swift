6b8f9839d04c9552fd31400a8f52efee9cce47a3
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-04-24 14:31:46 +0300

    struct sample

--- WissenSwiftUITutorial/Models/Student.swift
+++ WissenSwiftUITutorial/Models/Student.swift
@@ -3,8 +3,10 @@ import Foundation
 
 
 struct Student : Hashable {
-        var name : String = ""
-        var surname : String = ""
-        var address : String = ""
+    var name : String
+    var surname : String
+    var address : String = ""
+    var id : UUID = UUID()
+
 }
 
--- WissenSwiftUITutorial/StudentListSample.swift
+++ WissenSwiftUITutorial/StudentListSample.swift
@@ -13,15 +13,68 @@ struct StudentListSample: View {
     @State var students : [Student] = [
         Student(name: "Çağatay", surname: "Yıldız", address: "Çiyangir"),
         Student(name: "Sena", surname: "Burda", address: "Kağıthane"),
+        Student(name: "Sena", surname: "Orada", address: "Maslak"),
     ]
+    
+    @State var name = ""
+    @State var surname = ""
+    @State var address = ""
     
     var body: some View {
         VStack{
-            ForEach(students, id:\.self){ item in
-                Text(item.name)
+            
+            TextField("Name:",
+                      text: $name
+            )
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Surname:",
+                      text: $surname
+            )
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Address:",
+                      text: $address
+            ).padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Add"){
+                
+             
+                let newStudent : Student = Student(name: name, surname: surname, address: address)
+                
+                students.append(newStudent)
+                
+                name = ""
+                surname = ""
+                address = ""
+                
             }
+            .padding()
+            
+            Button("Remove All"){
+                students = []
+                // students.removeAll()
+            }
+            .padding()
+            
+            ForEach(students, id:\.self){ item in
+                //                Text(item.name + " " + item.surname + " " + item.address)
+                Text("\(item.name) \(item.surname) \(item.address) \(item.id)")
+                Button("Remove Student"){
+                    
+                    students = students.filter{$0.id != item.id}
+                    
+                }
+                .foregroundColor(.red)
+            
+            }
+            
+            Spacer()
         }
-       
+        
         
     }
 }
